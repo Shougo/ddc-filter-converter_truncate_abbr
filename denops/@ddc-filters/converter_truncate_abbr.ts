d2716afa8333586349f92666c302e2de9f222716
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-12-25 13:05:23 +0900

    Update deps

--- denops/@ddc-filters/converter_truncate_abbr.ts
+++ denops/@ddc-filters/converter_truncate_abbr.ts
@@ -1,7 +1,7 @@
 import {
   BaseFilter,
   Item,
-} from "https://deno.land/x/ddc_vim@v3.4.0/types.ts";
+} from "https://deno.land/x/ddc_vim@v4.3.1/types.ts";
 
 type Params = {
   maxAbbrWidth: number;
